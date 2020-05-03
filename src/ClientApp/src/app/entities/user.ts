88740b25cd7bdc31566cd378d042daaddd2be568
rmazza <robertmazza1@gmail.com>
2020-05-02 19:26:48 -0600

    added user class

--- src/ClientApp/src/app/entities/user.ts
+++ src/ClientApp/src/app/entities/user.ts
@@ -1,1 +1,18 @@
-﻿
+export class User {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  constructor(fname: string, mname: string, lname: string) {
+    this.firstName = fname;
+    this.middleName = mname;
+    this.lastName = lname;
+  }
+
+  getFullname() {
+    let name: string = `${this.firstName}`;
+    if (!this.middleName) {
+      name += ` ${this.middleName}`;
+    }
+    return `${name} ${this.lastName}`
+  }
+}
