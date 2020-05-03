6f4dde6aafc324a95364223264c44efa3500b894
rmazza <robertmazza1@gmail.com>
2020-05-02 20:09:32 -0600

    added graphql schema

---
+++ src/Controllers/GraphqlController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCRM.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class GraphqlController : ControllerBase
+    {
+    }
+}
