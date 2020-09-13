26197401ea807e7e01eadc839ac17405bf0053ba
rmazza <robertmazza1@gmail.com>
2020-09-12 20:01:07 -0600

    clean up

--- src/ClientApp/src/app/customers/customers.component.ts
+++ src/ClientApp/src/app/customers/customers.component.ts
@@ -57,20 +57,12 @@ export class CustomersComponent implements OnInit {
   loadCustomers(event: LazyLoadEvent) {  
     this.loading = true;
 
-    //in a real application, make a remote request to load data using state metadata from event
-    //event.first = First row offset
-    //event.rows = Number of rows per page
-    //event.sortField = Field name to sort with
-    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
-    //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
-
-    //imitate db connection over a network
     setTimeout(() => {
         if (this.datasource) {
             this.customers = this.datasource;
             this.loading = false;
         }
-    }, 1000);
+    }, 500);
   }
 
   confirmDelete(customer: Customer) {
