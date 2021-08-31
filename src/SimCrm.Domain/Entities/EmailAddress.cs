3b1869708e43b515c141962988d62f394cd11ecc
rmazza <robertmazza1@gmail.com>
2021-08-30 19:26:41 -0600

    added table name to email address

--- src/SimCrm.Domain/Entities/EmailAddress.cs
+++ src/SimCrm.Domain/Entities/EmailAddress.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SimCrm.Domain.Entities
 {
+    [Table("email_address")]
     public class EmailAddress : IDatabaseTable
     {
         [Column("eml_id")]
