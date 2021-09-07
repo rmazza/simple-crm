6524da4207e7aa17a868a55ed8bccef6e3944ae9
rmazza <robertmazza1@gmail.com>
2021-09-07 10:16:23 -0600

    added SetUp.cs to IntegrationTests

---
+++ tests/Application.IntegrationTests/SetUp.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests
+{
+    [SetUpFixture]
+    public class SetUp
+    {
+        [OneTimeSetUp]
+        public void RunFirst()
+        {
+
+        }
+    }
+}
